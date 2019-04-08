f9ff45edf16f53c2584bd15fe3f29c0704aca76e
theo <theo@vandersluijs.nl>
2019-04-08 14:29:12 -0400

    new

---
+++ medisana.py
@@ -0,0 +1,16 @@
+import requests
+
+start_url = "https://cloud.vitadock.com/signin"
+
+
+j_username = "theo@vandersluijs.nl"
+j_password = ""
+_spring_security_remember_me
+oauth_token
+marketingid
+code
+loginBtn  "Log in"
+_csrf = "csrf"
+
+data = {"j_username": j_username, "j_password": j_password, "_spring_security_remember_me": "", "oauth_token": "", "marketingid": "", "code": "", "loginBtn": "Log in", "_csrf": csrf  }
+
